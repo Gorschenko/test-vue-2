027320f706bd220823cc6df3badbfe494bc61edd
Gorschenko <aa.gorschenko@yandex.ru>
2022-01-23 12:29:51 +0300

    Добавлен компонент Header. Измненено изображение курсора согласно верстке. Добавлен первый функционал: валидация формы, добавление товара

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-<div class="wrapper">
-  <main class="main">
-    <AppForm class="main__form"/>
-    <AppList class="main__list"/>
-  </main>
-</div>
+  <div class="wrapper">
+    <TheHeader/>
+    <main class="main">
+      <AppForm
+        class="main__form"
+        @add-product="addProduct"
+      />
+      <AppList
+        class="main__list"
+        :products="products"
+      />
+    </main>
+  </div>
 </template>
 <script>
+import TheHeader from './components/TheHeader'
 import AppForm from './components/AppForm'
 import AppList from './components/AppList'
+import {ref} from 'vue'
 
 export default {
-components: { AppForm, AppList }
+  setup() {
+    const products = ref([])
+    const addProduct = product => {
+      console.log(product)
+      products.value.push(product)
+      console.log(products.value)
+    }
+    return {
+      products,
+      addProduct
+    }
+  },
+  components: { TheHeader, AppForm, AppList }
 }
 </script>
 
@@ -28,22 +49,27 @@ components: { AppForm, AppList }
   font-family: 'Source Sans Pro', sans-serif;
 }
 
-.wrapper {
+body {
   width: 100%;
   min-height: 100vh;
   background: rgba(255, 254, 251, 0.8);
 }
 
-.main {
-  display: flex;
-  align-items: flex-start;
+.wrapper {
   max-width: 1440px;
   margin: 0 auto;
   padding: 32px;
+}
+
+.main {
+  display: flex;
+  align-items: flex-start;
+
   &__form {
     min-width: 332px;
     margin-right: 16px;
   }
+
   &__list {
     max-width: 1028px;
   }
